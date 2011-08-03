079723af50fe62bb2766eceb10032befe0873e50
ringmaster <a_github@midnightcircus.com>
2011-08-03 10:04:55 -0400

    Check for route first then method, and fix example code.

--- examples/app.js
+++ examples/app.js
@@ -4,8 +4,9 @@ $("/").get(function(request, response) {
 	$.writeFile("index.html");
 });
 
-$("/test").post(function(request, response, data) {
+$("/test").post(function(request, response, querystring, data) {
 	console.log(data);
+	$.write(data.sample);
 });
 
 $.start(8888, true);
